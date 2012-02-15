a390e2b36bebafad8ad5554452af7c2d4bba589b
Alexander Lubyagin <lubyagin@yandex.ru>
2012-02-15 19:30:36 +0400

    Fix: Time.h and faq.txt

--- Net/Core/Time.h
+++ Net/Core/Time.h
@@ -1,1 +1,13 @@
+#ifndef __LINK_TIME_H__
+#define __LINK_TIME_H__
+
+#ifdef _MFC_VER
 long long GetTimestamp();
+#endif
+
+#ifdef __LINUX__
+#include <time.h>
+uint64_t GetTimestamp();
+#endif
+
+#endif
