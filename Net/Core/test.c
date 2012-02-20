cd28d451dc9c03b7d2e12ae30a2de451b7ba2017
Alexander Lubyagin <lubyagin@yandex.ru>
2012-02-21 01:00:15 +0400

    Fix: test.c debug

--- Net/Core/test.c
+++ Net/Core/test.c
@@ -4,5 +4,18 @@ int main() {
 	InitPersistentMemoryManager();
 	OpenStorageFile("test.bin");
 	SetStorageFileMemoryMapping();
+
+//#define itself ((Link *)0)
+
+uint64_t isA = CreateLink(itself, itself, itself);
+uint64_t isNotA = CreateLink(itself, itself, isA);
+uint64_t link = CreateLink(itself, isA, itself);
+uint64_t thing = CreateLink(itself, isNotA, link);
+
+UpdateLink(isA, isA, isA, link); // После этого минимальное ядро системы можно считать сформированным
+
+DeleteLink(isA); // Одна эта операция удалит все 4 связи
+
+	CloseStorageFile();
 	return 0;
 }
