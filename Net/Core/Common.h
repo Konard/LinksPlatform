7996c58939c313185760f25d5942df90e4380f81
Alexander Lubyagin <lubyagin@yandex.ru>
2012-02-28 20:38:32 +0400

    Fix: Common.h, comments

--- Net/Core/Common.h
+++ Net/Core/Common.h
@@ -2,12 +2,12 @@
 #define __LINKS_COMMON_H__
 
 /*
+// для Visual Studio C++
 #pragma inline_recursion(on)
 #pragma inline_depth(2)
 */
 
-//#include <omp.h>
-
+// Проверка на 64-битность (если не так - выдается предупреждение (warning))
 // see http://stackoverflow.com/questions/163058/how-can-i-detect-if-im-compiling-for-a-64bits-architecture-in-c
 #if defined(_LP64) || defined(__amd64__) || defined(_M_X64)
 #else
@@ -22,6 +22,7 @@
 // индекс линка unused
 #define LINK_0 0
 
+// Определение 32-ти и 64-битных типов
 #if defined(_MSC_VER) || defined(__MINGW32__)
 typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
