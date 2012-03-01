67f8bf93851ac34ccb68714d8619f853161de671
Alexander Lubyagin <lubyagin@yandex.ru>
2012-03-01 23:44:45 +0400

    sbt, simple_add without maintain

--- Research/sbt/sbt.c
+++ Research/sbt/sbt.c
@@ -4,27 +4,54 @@
 #include <stdio.h> // printf
 
 #include <pthread.h> // pthread_mutex_*
-// узкое место при доступе из многих потоков - глобальная блокировка
+// узкое место при доступе из многих потоков - глобальная блокировка таблицы _nodes
 pthread_mutex_t _lock_nodes = PTHREAD_MUTEX_INITIALIZER;
 TNodeIndex _root_index = -1;
 TNode _nodes[SBT_MAX_NODES];
 TNodeIndex _n_nodes = 0;
 
-int SBT_Add(TNumber number) {
+int SBT_Add_At(TNumber number, TNodeIndex t) {
 	_nodes[_n_nodes].number = number;
-	if (_n_nodes > 0) {
-		_nodes[_n_nodes-1].left = -1;
-		_nodes[_n_nodes-1].right = _n_nodes;
+	if (_n_nodes <= 0) {
 		_nodes[_n_nodes].left = -1;
 		_nodes[_n_nodes].right = -1;
+		_nodes[_n_nodes].size = 1;
+		_root_index = 0;
+		_n_nodes++;
 	}
 	else {
-		_nodes[_n_nodes].left = -1;
-		_nodes[_n_nodes].right = -1;
-		_root_index = 0;
+		if(number < _nodes[t].number) {
+			if(_nodes[t].left == -1) {
+				_nodes[t].left = _n_nodes;
+				_nodes[_n_nodes].number = number;
+				_nodes[_n_nodes].left = -1;
+				_nodes[_n_nodes].right = -1;
+				_nodes[_n_nodes].size = 1;
+				_n_nodes++;
+			}
+			else {
+				SBT_Add_At(number, _nodes[t].left);
+			}
+		}
+		else {
+			if(_nodes[t].right == -1) {
+				_nodes[t].right = _n_nodes;
+				_nodes[_n_nodes].number = number;
+				_nodes[_n_nodes].left = -1;
+				_nodes[_n_nodes].right = -1;
+				_nodes[_n_nodes].size = 1;
+				_n_nodes++;
+			}
+			else {
+				SBT_Add_At(number, _nodes[t].right);
+			}
+		}
 	}
-	_n_nodes++;
 	return 0;
+}
+
+int SBT_Add(TNumber number) {
+	return SBT_Add_At(number, _root_index);
 }
 
 int SBT_Delete(TNumber n) {
@@ -33,11 +60,12 @@ int SBT_Delete(TNumber n) {
 
 void SBT_PrintAllNodes_At(int depth, TNodeIndex t) {
 	if ((_n_nodes <= 0) || (t < 0)) return; // выйти, если вершины нет
-	printf("%d "SBT_FORMAT_STRING" "SBT_FORMAT_STRING"\n", depth, t, (SBT_FORMAT_TYPE)_nodes[t].number); // иначе: напечатать "тело" узла
 	if (_nodes[t].left > 0) SBT_PrintAllNodes_At(depth + 1, _nodes[t].left);
+	printf("depth = %d, node = "SBT_FORMAT_STRING": ("SBT_FORMAT_STRING")\n", depth, t, (SBT_FORMAT_TYPE)_nodes[t].number); // иначе: напечатать "тело" узла
 	if (_nodes[t].right > 0) SBT_PrintAllNodes_At(depth + 1, _nodes[t].right);
 }
 
 void SBT_PrintAllNodes() {
-	SBT_PrintAllNodes_At(0,_root_index);
+	SBT_PrintAllNodes_At(0, _root_index);
+	printf("_n_nodes = "SBT_FORMAT_STRING"\n", _n_nodes);
 }
