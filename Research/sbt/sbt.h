6d7766562a5c910877df3854075ee7e19f916653
Alexander Lubyagin <lubyagin@yandex.ru>
2012-03-01 20:43:44 +0400

    Fix: error: variably modified ‘nodes’ at file scope

--- Research/sbt/sbt.h
+++ Research/sbt/sbt.h
@@ -1,7 +1,8 @@
 #ifndef __SBT_H__
 #define __SBT_H__
 
-const int SBT_MAX_NODES = 1000000; // 1 млн. узлов
+#define SBT_MAX_NODES 1000000
+// C language does not mean constant, const int SBT_MAX_NODES = 1000000; // 1 млн. узлов
 
 #include <stdint.h>
 typedef uint64_t TNumber;
