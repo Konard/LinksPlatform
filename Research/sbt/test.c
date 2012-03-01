b90a8bbda010d8bb1b9a723434ebb93402948cb5
Alexander Lubyagin <lubyagin@yandex.ru>
2012-03-01 23:26:30 +0400

    sbt, generate random nodes

--- Research/sbt/test.c
+++ Research/sbt/test.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#include <stdio.h> // printf
+#define __USE_XOPEN_EXTENDED
+#include <stdlib.h> // random
+#include <time.h> // time
 
 #include "sbt.h"
 
 int main() {
+	srandom(time(NULL)%1000);
 	for(int i = 0; i < 100; i++) {
-		SBT_Add(i);
+		SBT_Add(random() % 1000);
 	}
 	SBT_PrintAllNodes();
 	return 0;
