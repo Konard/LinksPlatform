1b46deb0649b30a6dcb3bf9e380b4a58e1442bf0
Alexander Lubyagin <lubyagin@yandex.ru>
2012-03-06 22:02:29 +0400

    sbt, pseudo-random for nodes

--- Research/sbt/test.c
+++ Research/sbt/test.c
@@ -6,20 +6,34 @@
 #include "sbt.h"
 
 int main() {
+
 /*
 	srandom(time(NULL)%1000);
-	for(int i = 0; i < 10; i++) {
+	for(int i = 0; i < 50; i++) {
 		SBT_Add(random() % 1000);
 		SBT_PrintAllNodes();
 	}
 */
+#define RND_SEED 100
+#define RND_A 9
+#define RND_B 9
+#define RND_C 7
+	int rnd = RND_SEED;
+	for(int i = 0; i < 50; i++) {
+		rnd ^= (rnd << RND_A);
+		rnd ^= (rnd >> RND_B);
+		rnd ^= (rnd << RND_C);
+		SBT_Add((rnd)&0x000000FF);
+		SBT_PrintAllNodes();
+	}
 
+/*
 	// второй пример
 	SBT_Add(1);
 	SBT_Add(2);
 	SBT_Add(3);
 	SBT_Add(4);
 	SBT_Add(5);
-
+*/
 	return 0;
 }
