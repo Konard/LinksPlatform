4da6f7c4ce88f0ff7836c4f6f72d352e72e7bd66
lubyagin <lubyagin@yandex.ru>
2012-03-16 14:19:22 +0400

    boost-val began

--- Research/boost-avl/test.cpp
+++ Research/boost-avl/test.cpp
@@ -1,6 +1,11 @@
 // see http://www.boost.org/doc/libs/1_43_0/doc/html/boost/intrusive/avltree.html
 // and package libboost-dev
 
+#include <boost/intrusive/avl_set.hpp>
+
+using namespace boost::intrusive;
+
 int main() {
+	
 	return 0;
 }
