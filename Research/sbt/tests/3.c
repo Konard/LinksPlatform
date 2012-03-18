12fb463e1f15e62d23a567512795e8baa552576c
lubyagin <lubyagin@yandex.ru>
2012-03-18 20:50:48 +0400

    sbt, 3.c test commented

--- Research/sbt/tests/3.c
+++ Research/sbt/tests/3.c
@@ -8,20 +8,24 @@
 int main() {
 
 	// третий пример
+	// генерация псевдослучайных чисел
 #define RND_SEED 100
 #define RND_A 9
 #define RND_B 9
 #define RND_C 7
 	int rnd = RND_SEED;
-	for(int i = 0; i < 1000000; i++) {
+	for(int i = 0; i < 10000000; i++) {
 		rnd ^= (rnd << RND_A);
 		rnd ^= (rnd >> RND_B);
 		rnd ^= (rnd << RND_C);
-		SBT_AddNodeUniq((rnd)&0x000000FF);
+		//SBT_AddNodeUniq((rnd)&0x000000FF); // вставка с отказами
+		SBT_AddNode((rnd)&0x000000FF); // вставка без отказов
 	}
-	SBT_CheckAllNodes();
-	SBT_PrintAllNodes();
-	SBT_DumpAllNodes();
+
+	// результат работы
+//	SBT_CheckAllNodes();
+//	SBT_PrintAllNodes();
+//	SBT_DumpAllNodes();
 
 	return 0;
 }
