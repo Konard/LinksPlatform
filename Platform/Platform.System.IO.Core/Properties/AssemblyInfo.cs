8cf16032c667dc9e0621fa04ea9701cc0c505645
Константин Дьяченко <konard@me.com>
2015-10-22 17:04:20 +0300

    Renaming Platform.Links.FileSystem.Core -> Platform.System.IO.Core

--- Platform/Platform.System.IO.Core/Properties/AssemblyInfo.cs
+++ Platform/Platform.System.IO.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Platform.Links.FileSystem.Core")]
+[assembly: AssemblyTitle("Platform.System.IO.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Platform.Links.FileSystem.Core")]
+[assembly: AssemblyProduct("Platform.System.IO.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
