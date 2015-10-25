8d49383873c25570d6ee0eed1aabb1bfad6ca546
Константин Дьяченко <konard@me.com>
2015-10-25 06:35:16 +0300

    Added Windows script to run Binaries.

---
+++ Binaries/run.bat
@@ -0,0 +1,2 @@
+start "" "MasterServer\Application Files\Platform.Data.MasterServer_0_0_0_3\Platform.Data.MasterServer.exe"
+start "" "ConsoleTerminal\Application Files\Platform.Data.ConsoleTerminal_0_0_0_3\Platform.Data.ConsoleTerminal.exe"
