2b8a7d8a2eac5133ad482414eef4bd656e40afbe
Konstantin Dyachenko <drakonard@gmail.com>
2015-10-25 03:33:09 +0000

    Added Linux scripts for running Binaries.

---
+++ Binaries/run.server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mono "MasterServer/Application Files/Platform.Data.MasterServer_0_0_0_3/Platform.Data.MasterServer.exe"
---
+++ Binaries/run.terminal.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mono "ConsoleTerminal/Application Files/Platform.Data.ConsoleTerminal_0_0_0_3/Platform.Data.ConsoleTerminal.exe"
