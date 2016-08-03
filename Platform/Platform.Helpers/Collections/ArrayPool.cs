3d452dc92bc1c91f343fe13bfdf24e8b81813c43
Константин Дьяченко <konard@me.com>
2016-08-04 00:14:43 +0300

    Added limitation on minimal dictionary size and maximum stack sizes for ArrayPool. This should improve performance and reduce possible memory leaks (not tested). Default now contains centralized mechanism for ThreadStatic default instances.

--- Platform/Platform.Helpers/Collections/ArrayPool.cs
+++ Platform/Platform.Helpers/Collections/ArrayPool.cs
@@ -5,37 +5,41 @@ using Platform.Helpers.Disposables;
 
 namespace Platform.Helpers.Collections
 {
+    /// <remarks>
+    /// TODO: Check actual performance
+    /// TODO: Check for memory leaks
+    /// </remarks>
     public static class ArrayPool
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T[] Allocate<T>(Integer size)
-        {
-            var arrayPool = ArrayPool<T>.ThreadDefault ?? (ArrayPool<T>.ThreadDefault = new ArrayPool<T>());
-            return arrayPool.Allocate(size);
-        }
+        public static T[] Allocate<T>(Integer size) => Default.GetOrCreateThreadInstance<ArrayPool<T>>().Allocate(size);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void Free<T>(T[] array)
-        {
-            var arrayPool = ArrayPool<T>.ThreadDefault ?? (ArrayPool<T>.ThreadDefault = new ArrayPool<T>());
-            arrayPool.Free(array);
-        }
+        public static void Free<T>(T[] array) => Default.GetOrCreateThreadInstance<ArrayPool<T>>().Free(array);
     }
 
     /// <remarks>
     /// Original idea from http://geekswithblogs.net/blackrob/archive/2014/12/18/array-pooling-in-csharp.aspx
     /// </remarks>
-    /// <remarks>
-    /// TODO: Check actual performance
-    /// TODO: Check for memory leaks
-    /// </remarks>
     public class ArrayPool<T>
     {
-        [ThreadStatic] public static ArrayPool<T> ThreadDefault;
+        public const int DefaultSizesAmount = 512;
+        public const int DefaultMaxArraysPerSize = 32;
 
-        private readonly Dictionary<int, Stack<T[]>> _pool = new Dictionary<int, Stack<T[]>>();
-
+        private readonly int _maxArraysPerSize;
         public static readonly T[] Empty = new T[0];
+
+        private readonly Dictionary<int, Stack<T[]>> _pool = new Dictionary<int, Stack<T[]>>(DefaultSizesAmount);
+
+        public ArrayPool(int maxArraysPerSize)
+        {
+            _maxArraysPerSize = maxArraysPerSize;
+        }
+
+        public ArrayPool()
+           : this(DefaultMaxArraysPerSize)
+        {
+        }
 
         public Disposable<T[]> AllocateDisposable(long size) => Disposable<T[]>.Create(Allocate(size), Free);
 
@@ -63,7 +67,10 @@ namespace Platform.Helpers.Collections
             Ensure.ArgumentNotNull(array, nameof(array));
             if (array.Length == 0)
                 return;
-            _pool.GetOrAdd(array.Length, size => new Stack<T[]>()).Push(array);
+            var stack = _pool.GetOrAdd(array.Length, size => new Stack<T[]>(_maxArraysPerSize));
+            if (stack.Count == _maxArraysPerSize)
+                return; // Do not put the array to stack
+            stack.Push(array);
         }
     }
 }
--- Platform/Platform.Helpers/Default.cs
+++ Platform/Platform.Helpers/Default.cs
@@ -1,4 +1,7 @@
-﻿namespace Platform.Helpers
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Platform.Helpers
 {
     /// <summary>
     /// Представляет собой точку доступа к экземплярям типов по умолчанию (созданных с помощью конструктора без аргументов).
@@ -7,6 +10,31 @@
     public class Default<T>
         where T : new()
     {
+        /// <summary>
+        /// Default.GetOrCreateThreadInstance methods are recommended instead.
+        /// Use direclty only if you understand what you are doing (see remarks for hint).
+        /// </summary>
+        /// <remarks>
+        /// If you really need maximum performance, use this field together with Default.InitializeThreadInstance method.
+        /// This method should be called only once per thread, but you can call multiple times if you need to replace the instance with new one.
+        /// </remarks>
+        [ThreadStatic]
+        public static T ThreadInstance;
+
         public static readonly T Instance = new T();
+    }
+
+    public class Default
+    {
+        public static void InitializeThreadInstance<T>()
+            where T : new() => Default<T>.ThreadInstance = new T();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T GetOrCreateThreadInstance<T>()
+            where T : class, new() => Default<T>.ThreadInstance ?? (Default<T>.ThreadInstance = new T());
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T GetOrCreateThreadInstance<T>(T nullValue)
+            where T : struct => Equals(Default<T>.ThreadInstance, nullValue) ? (Default<T>.ThreadInstance = new T()) : Default<T>.ThreadInstance;
     }
 }
