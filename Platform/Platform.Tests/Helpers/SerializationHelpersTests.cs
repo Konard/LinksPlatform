12d0a7c6366c6a3b2d3c5f6d88740ba63e02a26d
Eugene Katsevman <eugene.katsevman@gmail.com>
2016-09-24 22:04:17 +0300

    newline chars fixed, assert.equal instead of assert.true

--- Platform/Platform.Tests/Helpers/SerializationHelpersTests.cs
+++ Platform/Platform.Tests/Helpers/SerializationHelpersTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using Platform.Helpers;
 using Xunit;
+using System;
 
 namespace Platform.Tests.Helpers
 {
@@ -13,7 +14,7 @@ namespace Platform.Tests.Helpers
 
             SerializationHelpers.SerializeToXmlFile(Default<object>.Instance, tempFilename);
 
-            Assert.True(File.ReadAllText(tempFilename) == "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<anyType xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />");
+            Assert.Equal(File.ReadAllText(tempFilename), "<?xml version=\"1.0\" encoding=\"utf-8\"?>"+System.Environment.NewLine+"<anyType xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />");
 
             File.Delete(tempFilename);
         }
@@ -22,7 +23,7 @@ namespace Platform.Tests.Helpers
         public void SerializeAsXmlStringTest()
         {
             var serializedObject = SerializationHelpers.SerializeToXmlString(Default<object>.Instance);
-            Assert.True(serializedObject == "<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<anyType xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />");
+            Assert.Equal(serializedObject, "<?xml version=\"1.0\" encoding=\"utf-16\"?>"+System.Environment.NewLine+"<anyType xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />");
         }
     }
 }
