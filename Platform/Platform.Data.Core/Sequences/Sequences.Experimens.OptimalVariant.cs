e905943f55357c62ca56af226a80de7d672d5994
Konstantin Dyachenko <drakonard@gmail.com>
2017-12-02 14:06:32 +0300

    Create Sequences.Experimens.OptimalVariant.cs

---
+++ Platform/Platform.Data.Core/Sequences/Sequences.Experimens.OptimalVariant.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Platform.Data.Core.Collections;
+using Platform.Data.Core.Exceptions;
+using Platform.Data.Core.Pairs;
+using Platform.Helpers;
+using Platform.Helpers.Collections;
+using LinkIndex = System.UInt64;
+
+namespace Platform.Data.Core.Sequences
+{
+    partial class Sequences
+    {
+        
+    }
+}
