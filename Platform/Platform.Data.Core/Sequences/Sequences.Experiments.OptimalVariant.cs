8b3af8c46d830030b56493e8a4f93c79570d5ae6
Konstantin Dyachenko <drakonard@gmail.com>
2017-12-02 14:37:45 +0300

    Update Sequences.Experiments.OptimalVariant.cs

--- Platform/Platform.Data.Core/Sequences/Sequences.Experiments.OptimalVariant.cs
+++ Platform/Platform.Data.Core/Sequences/Sequences.Experiments.OptimalVariant.cs
@@ -13,6 +13,26 @@ namespace Platform.Data.Core.Sequences
 {
     partial class Sequences
     {
+        public bool IncrementPairsFrequencies(ulong[] sequence)
+        {
+            var indexed = true;
+
+            var i = sequence.Length;
+            //while (--i >= 1 && (indexed = Links.SearchOrDefault(sequence[i - 1], sequence[i]) != Constants.Null)) { }
+
+            for (; i >= 1; i--)
+                Links.GetOrCreate(sequence[i - 1], sequence[i]);
+
+            return indexed;
+        }
         
+        public void IncrementPairFrequency(ulong source, ulong target)
+        {
+            var pair = Links.GetOrCreate(source, target);
+            
+            var frequency = GetPairFrequency(pair);
+            frequency = IncrementPairFrequency(frequency);
+            SetPairFrequency(pair, frequency);
+        }
     }
 }
