5330c5e3f66b3ed4f93a6d351c4ea9e2cf206609
Konstantin Dyachenko <drakonard@gmail.com>
2017-12-09 08:17:11 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,12 @@ TARGET_BRANCH="gh-pages"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
+    echo "Skipping deploy."
     exit 0
 fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
@@ -26,7 +24,7 @@ cd ..
 # Clean out existing contents
 rm -rf out/**/* || exit 0
 
-# Run our compile script
+# Copy genereted docs site
 cp -r doc/generated/site/* out
 
 # Now let's go have some fun with the cloned repo
@@ -48,4 +46,4 @@ git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git push $TARGET_BRANCH
