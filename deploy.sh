8bbe6da61bb1e6f4536c941a2deb2039ba241c24
Konstantin Dyachenko <drakonard@gmail.com>
2017-12-09 08:07:46 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
-function doCompile {
-  ./compile.sh
-}
-
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
@@ -31,7 +27,7 @@ cd ..
 rm -rf out/**/* || exit 0
 
 # Run our compile script
-doCompile
+cp -r doc/generated/site/* out
 
 # Now let's go have some fun with the cloned repo
 cd out
