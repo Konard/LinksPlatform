a7a74d7dd33dc98613b9b541fb1c8a0089b0c325
Константин Дьяченко <konard@me.com>
2019-03-11 22:01:47 +0300

    Update to .NET Core 2.2

--- Platform/Platform.Tests/Helpers/SerializationHelpersTests.cs
+++ Platform/Platform.Tests/Helpers/SerializationHelpersTests.cs
@@ -17,7 +17,7 @@ namespace Platform.Tests.Helpers
 #if NET45
             Assert.Equal(File.ReadAllText(tempFilename), $"<?xml version=\"1.0\"?>{Environment.NewLine}<anyType xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" />");
 #else
-            Assert.Equal(File.ReadAllText(tempFilename), $"<?xml version=\"1.0\" encoding=\"utf-8\"?>{Environment.NewLine}<anyType xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />");
+            Assert.Equal(File.ReadAllText(tempFilename), $"<?xml version=\"1.0\"?>{Environment.NewLine}<anyType xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />");
 #endif
 
             File.Delete(tempFilename);
