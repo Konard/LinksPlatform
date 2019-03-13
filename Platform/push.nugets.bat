3d618e200d0bea8885b8d84fafd98ed468ccfc2b
Константин Дьяченко <konard@me.com>
2019-03-13 22:41:17 +0300

    Nugets script updated no more Platform.Tests and Platform.WindowsAPI nugets.

--- Platform/push.nugets.bat
+++ Platform/push.nugets.bat
@@ -1,11 +1,3 @@
-cd Platform.WindowsAPI
-dotnet pack -c Release
-cd bin\Release
-nuget push *.symbols.nupkg
-del *.symbols.nupkg
-nuget push *.nupkg
-del *.nupkg
-cd ..\..\..
 cd Platform.Helpers
 dotnet pack -c Release
 cd bin\Release
@@ -31,14 +23,6 @@ nuget push *.nupkg
 del *.nupkg
 cd ..\..\..
 cd Platform.Data.Core
-dotnet pack -c Release
-cd bin\Release
-nuget push *.symbols.nupkg
-del *.symbols.nupkg
-nuget push *.nupkg
-del *.nupkg
-cd ..\..\..
-cd Platform.Tests
 dotnet pack -c Release
 cd bin\Release
 nuget push *.symbols.nupkg
