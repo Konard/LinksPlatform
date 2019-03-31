8204948ad9d263dbb2921d1b018fa1dbbc77bb03
Konstantin Dyachenko <konard@me.com>
2019-03-31 22:41:33 +0300

    Update ICollectionExtensions.cs

--- Platform/Platform.Helpers/Collections/ICollectionExtensions.cs
+++ Platform/Platform.Helpers/Collections/ICollectionExtensions.cs
@@ -7,11 +7,8 @@ namespace Platform.Helpers.Collections
     {
         public static bool IsNullOrEmpty<T>(this ICollection<T> collection) => collection == null || collection.Count == 0;
 
-        public static bool IsNullOrEmptyOrAllEqualToDefault<T>(this ICollection<T> collection)
+        public static bool AllEqualToDefault<T>(this ICollection<T> collection)
         {
-            if (collection.IsNullOrEmpty())
-                return true;
-
             var comparer = EqualityComparer<T>.Default;
             return collection.All(item => comparer.Equals(item, default(T)));
         }
