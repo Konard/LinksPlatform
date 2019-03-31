17cf72e68450f687f0d291ecd4254dde59ecc868
Konstantin Dyachenko <konard@me.com>
2019-03-31 22:08:20 +0300

    Update StackExtensions.cs

--- Platform/Platform.Helpers/Collections/StackExtensions.cs
+++ Platform/Platform.Helpers/Collections/StackExtensions.cs
@@ -6,6 +6,6 @@ namespace Platform.Helpers.Collections
     public static class StackExtensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static T PopOrDefault<T>(this Stack<T> stack) stack.Count > 0 ? stack.Pop() : default(T);
+        public static T PopOrDefault<T>(this Stack<T> stack) => stack.Count > 0 ? stack.Pop() : default(T);
     }
 }
