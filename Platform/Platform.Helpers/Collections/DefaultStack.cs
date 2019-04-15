35e671f213be867b9a8202e565ca7158cc4e3376
Константин Дьяченко <konard@me.com>
2019-04-15 22:44:20 +0300

    Added DefaultStack as an default IStack implementation to Platform.Helpers.Collections.

---
+++ Platform/Platform.Helpers/Collections/DefaultStack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Platform.Helpers.Collections
+{
+    public class DefaultStack<TElement> : IStack<TElement>
+    {
+        private readonly Stack<TElement> _stack;
+
+        public DefaultStack() => _stack = new Stack<TElement>();
+
+        public TElement Peek() => _stack.Peek();
+
+        public TElement Pop() => _stack.Pop();
+
+        public void Push(TElement element) => _stack.Push(element);
+    }
+}
