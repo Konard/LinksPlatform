1a5f7440ae4450551380aa9080c89395f4e1901c
Константин Дьяченко <konard@me.com>
2019-04-23 12:37:49 +0300

    DictionaryBasedLinkFrequencyIncrementer now returns link instead of frequency.

--- Platform/Platform.Data.Core/Common/DictionaryBasedLinkFrequencyIncrementer.cs
+++ Platform/Platform.Data.Core/Common/DictionaryBasedLinkFrequencyIncrementer.cs
@@ -9,20 +9,20 @@ namespace Platform.Data.Core.Common
 
         public DictionaryBasedLinkFrequencyIncrementer(IDictionary<TLink, TLink> linksToFrequencies) => _linksToFrequencies = linksToFrequencies;
 
+        /// <remarks>Link itseft is not changed, only it's frequency is incremented.</remarks>
         public TLink Increment(TLink link)
         {
             if (_linksToFrequencies.TryGetValue(link, out TLink frequency))
             {
                 frequency = MathHelpers.Increment(frequency);
                 _linksToFrequencies[link] = frequency;
-                return frequency;
             }
             else
             {
                 frequency = Integer<TLink>.One;
                 _linksToFrequencies.Add(link, frequency);
-                return frequency;
             }
+            return link;
         }
     }
 }
