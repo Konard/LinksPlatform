42c2ce6b9d9897f0d236743883c72f653207d270
Константин Дьяченко <konard@me.com>
2019-05-26 22:36:57 +0300

    IDictionaryExtensions refactored and simplified.

--- Platform/Platform.Helpers/Collections/IDictionaryExtensions.cs
+++ Platform/Platform.Helpers/Collections/IDictionaryExtensions.cs
@@ -9,15 +9,14 @@ namespace Platform.Helpers.Collections
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
         {
-            TValue value;
-            return dictionary.TryGetValue(key, out value) ? value : default;
+            dictionary.TryGetValue(key, out TValue value);
+            return value;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> valueFactory)
         {
-            TValue value;
-            if (!dictionary.TryGetValue(key, out value))
+            if (!dictionary.TryGetValue(key, out TValue value))
             {
                 value = valueFactory(key);
                 dictionary.Add(key, value);
