d8d65c2ae932cb88d3f77d541caa395c70de4aa5
Константин Дьяченко <konard@me.com>
2019-07-17 18:46:24 +0300

    Fixed using for Platform.Helpers.Console namespace

--- Platform/Platform.Data.Core/Collections/BitString.cs
+++ Platform/Platform.Data.Core/Collections/BitString.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using Platform.Helpers;
+using Platform.Helpers.Console;
 
 // ReSharper disable ForCanBeConvertedToForeach
 
