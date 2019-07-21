05981a23e7584f15c400acd6d1bd4d3f3c98530c
Константин Дьяченко <konard@me.com>
2019-07-21 17:05:39 +0300

    Platform.Helpers project moved to separate repository https://github.com/linksplatform/Helpers and is available as Platform.Helpers package at nuget.org

--- Platform/Platform.Helpers/HashHelpers.cs
+++
@@ -1,9 +0,0 @@
-﻿using Platform.Collections.Lists;
-
-namespace Platform.Helpers
-{
-    public static class HashHelpers 
-    {
-        public static int Generate<T>(params T[] hashCodes) => hashCodes.GenerateHashCode();
-    }
-}
