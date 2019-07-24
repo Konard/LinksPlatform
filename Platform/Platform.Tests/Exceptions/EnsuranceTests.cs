b3b1b9d90790083d5adf90b757ad616c0c73d698
Константин Дьяченко <konard@me.com>
2019-07-24 21:50:52 +0300

    Added tests for new Ensure mechanism from Platform.Exceptions.

---
+++ Platform/Platform.Tests/Exceptions/EnsuranceTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+using Platform.Exceptions;
+
+namespace Platform.Tests.Exceptions
+{
+    public static class EnsuranceTests
+    {
+        [Fact]
+        public static void ArgumentNotNullEnsuranceTest()
+        {
+            // Should throw an exception (even if in neighbour "Ignore" namespace it was overridden, but here this namespace is not used)
+            Assert.Throws<ArgumentNullException>(() => Ensure.Always.ArgumentNotNull<object>(null, "object"));
+        }
+    }
+}
---
+++ Platform/Platform.Tests/Exceptions/Ignore/EnsureAlwaysExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Platform.Exceptions;
+
+namespace Platform.Tests.Exceptions.Ignore
+{
+    public static class EnsureAlwaysExtensions
+    {
+        [Conditional("DEBUG")]
+        public static void ArgumentNotNull<TArgument>(this EnsureAlwaysExtensionRoot root, TArgument argument, string argumentName)
+            where TArgument : class
+        {
+        }
+    }
+}
