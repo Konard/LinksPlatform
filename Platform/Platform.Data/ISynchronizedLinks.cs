8123ccd3cef6f3155865e28cb4098ec6fc861f3d
Константин Дьяченко <konard@me.com>
2019-07-29 14:18:51 +0300

    TODO comment removed.

--- Platform/Platform.Data/ISynchronizedLinks.cs
+++ Platform/Platform.Data/ISynchronizedLinks.cs
@@ -3,7 +3,6 @@ using Platform.Data.Constants;
 
 namespace Platform.Data
 {
-    // TODO: Move to Platform.Data
     public interface ISynchronizedLinks<TLink, TLinks, TConstants> : ISynchronized<TLinks>, ILinks<TLink, TConstants>
         where TConstants : ILinksCombinedConstants<TLink, TLink, int, TConstants>
         where TLinks : ILinks<TLink, TConstants>
